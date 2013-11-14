b191d2ec40a403727b23cb57012d8b1a21aec6db
Sandeep Raghuraman <sandy.8925@gmail.com>
2013-11-13 22:37:48 -0800

    atm.py: Rename bank_charge to bank_transaction_charge

--- atm.py
+++ atm.py
@@ -1,12 +1,12 @@
 import re
 input_data = re.split('\s',raw_input())
 
-bank_charge = 0.50
+bank_transaction_charge = 0.50
 
 withdraw_amount = int(input_data[0])
 bank_balance = float(input_data[1])
 
-debit_amount = withdraw_amount + bank_charge
+debit_amount = withdraw_amount + bank_transaction_charge
 
 if withdraw_amount%5==0 and debit_amount <= bank_balance:
   bank_balance -= debit_amount
