4e9dfc4dc6b7ff88622696dd3f6ba0f3643222a9
Sandeep Raghuraman <sandy.8925@gmail.com>
2013-11-15 23:43:52 -0800

    Correct program: Use split to parse input. Explicitly declare bank_transaction_charge as float. Remove input_data, no longer needed. Remove exit statement

--- atm.py
+++ atm.py
@@ -1,10 +1,9 @@
-import re
-input_data = re.split('\s',raw_input())
+withdraw_amount,bank_balance = raw_input().split()
 
-bank_transaction_charge = 0.50
+bank_transaction_charge = float(0.50)
 
-withdraw_amount = int(input_data[0])
-bank_balance = float(input_data[1])
+withdraw_amount = int(withdraw_amount)
+bank_balance = float(bank_balance)
 
 debit_amount = withdraw_amount + bank_transaction_charge
 
@@ -12,5 +11,3 @@ if withdraw_amount%5==0 and debit_amount<=bank_balance:
   bank_balance -= debit_amount
 
 print ("%.2f" % bank_balance)
-
-exit(0)
